80e52f06abc48a8d2c9fc1d69a394e4599339d9f
Pascal Meßmer <p.m.messmer@googlemail.com>
2022-03-02 21:34:32 +0100

    V0.3

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,23 @@
-
-
 //HIDING AND UNHIDING DIVS BEHIND INPUTS:
 
-var checkedDivs = ["trickery", "adaptation", "gods", "magic", "metal", "ranged", "deceitful", "grp", "externar", "construct", "terrible"];
-for (let x of checkedDivs) {
+var HiddenStuff = ["trickery", "adaptation", "gods", "magic", "metal", "ranged", "deceitful", "grp", "externar", "construct", "terrible"];
+for (let x of HiddenStuff) {
   $("." + x + "Div").hide();
 }
 //initially hide text behind checkboxes
-for (let x of checkedDivs) {
+
+
+for (let x of HiddenStuff) {
   $("#" + x + "Check").click(function() {
     $("." + x + "Div").toggle();
+
+    if ($("#" + x + "Input").prop('required') !== true) {
+      $("#" + x + "Input").attr('required', 'required')
+    } else {
+      $("#" + x + "Input").removeAttr('required')
+    }
+
   })
 }
 //toggle hiding unhiding text behind checkboxes
+//toggle required on hidden textboxes
